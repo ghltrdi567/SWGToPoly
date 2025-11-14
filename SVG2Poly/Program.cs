b3de6d0a8bf55b0449a67f9e944d041815aa47b2
ilyvlsmirusaddit <iluka333@yandex.ru>
2025-11-14 10:56:48 +0300

    added conversion files in input directory

--- SVG2Poly/Program.cs
+++ SVG2Poly/Program.cs
@@ -1,4 +1,5 @@
-﻿using EarClipperLib;
+﻿using Clipper2Lib;
+using EarClipperLib;
 using Svg;
 using Svg.Pathing;
 using SVG2Poly.Helpers;
@@ -6,6 +7,15 @@ using SVG2Poly.SWGPath;
 using SVG2Poly.SWGPathArranger;
 using System.Numerics;
 using System.Text;
+
+
+Paths64 subj = new Paths64();
+Paths64 clip = new Paths64();
+subj.Add(Clipper.MakePath(new int[] { 100, 50, 10, 79, 65, 2, 65, 98, 10, 21 }));
+clip.Add(Clipper.MakePath(new int[] { 98, 63, 4, 68, 77, 8, 52, 100, 19, 12 }));
+Paths64 solution = Clipper.Intersect(subj, clip, FillRule.NonZero);
+
+
 
 
 
